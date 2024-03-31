86637af4af9fe7ce51d3ef9e4dd41686d0f42b97
Shamim <shossain@secureship.ca>
2024-03-31 06:51:12 +0600

    feat: add pocketbase client

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import PocketBase from 'pocketbase';
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			locale: import('$lib/i18n/i18n-types.ts').Locales;
+			pb: PocketBase;
 		}
 		// interface PageData {}
 		// interface Platform {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,8 @@ import type { Handle } from '@sveltejs/kit';
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors';
 import { detectLocale } from '$lib/i18n/i18n-util.js';
 import { sequence } from '@sveltejs/kit/hooks';
+import PocketBase from 'pocketbase';
+import { POCKETBASE_URL } from '$env/static/private';
 
 async function urlRewrite({ event, resolve }) {
 	if (event.url.pathname.match(/[A-Z]/)) {
@@ -20,6 +22,28 @@ async function i18n({ event, resolve }) {
 	return resolve(event, {
 		transformPageChunk: ({ html }) => html.replace('%lang%', locale)
 	});
+}
+
+async function initPocketbase({ event, resolve }) {
+	event.locals.pb = new PocketBase(POCKETBASE_URL);
+
+	// load the store data from the request cookie string
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	try {
+		// get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+		event.locals.pb.authStore.isValid && (await event.locals.pb.collection('users').authRefresh());
+	} catch (_) {
+		// clear the auth store on failed refresh
+		event.locals.pb.authStore.clear();
+	}
+
+	const response = await resolve(event);
+
+	// send back the default 'pb_auth' cookie to the client with the latest store state
+	response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie());
+
+	return response;
 }
 
 export const handle = sequence(urlRewrite, i18n) satisfies Handle;
