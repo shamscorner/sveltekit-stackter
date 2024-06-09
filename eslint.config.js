96f19717eee24d2157e676fb4399ae9a0c0c8765
Shamim <shossain@secureship.ca>
2024-06-09 10:06:17 +0600

    chore: update deps

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: ts.parser
+			}
+		}
+	},
+	{
+		ignores: [
+			'build/',
+			'.svelte-kit/',
+			'.vercel/',
+			'test-results/',
+			'dist/',
+			'.DS_Store',
+			'src/lib/components/common/Analytics.svelte',
+			'pocketbase/'
+		]
+	},
+	{
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'off',
+			'svelte/no-at-html-tags': 'off',
+			'@typescript-eslint/no-unused-vars': 'off'
+		}
+	}
+];
