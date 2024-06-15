e7576b2c8ae226fef29a6c543e9d0765cf556d4e
Shamim <shossain@secureship.ca>
2024-06-15 11:38:48 +0600

    fix: typescript issues

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,6 @@
 // for information about these interfaces
 
 import PocketBase from 'pocketbase';
-import type { DefaultSession } from '@auth/sveltekit';
 
 declare global {
 	namespace App {
@@ -10,17 +9,7 @@ declare global {
 		interface Locals {
 			locale: import('$lib/i18n/i18n-types.ts').Locales;
 			pb: PocketBase;
-			session: {
-				user: {
-					userId: string;
-					/**
-					 * By default, TypeScript merges new interface properties and overwrites existing ones.
-					 * In this case, the default session user properties will be overwritten,
-					 * with the new ones defined above. To keep the default session user properties,
-					 * you need to add them back into the newly declared interface.
-					 */
-				} & DefaultSession['user'];
-			};
+			session: Session | null | undefined;
 		}
 		// interface PageData {}
 		// interface Platform {}
